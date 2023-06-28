b41f79d02c8574e846329ba3f2c4031f7b317155
Marcin Wozniak <marcindamianwozniak@gmail.com>
2023-06-28 10:10:34 +0200

    feat(middleware): Add token check

--- middleware.ts
+++ middleware.ts
@@ -1,3 +1,17 @@
+import { NextRequest, NextResponse } from "next/server"
+
 export { default } from "next-auth/middleware"
+
+export function middleware(request: NextRequest) {
+
+    if((request.nextUrl.searchParams.get('workspace') || request.cookies.has('workspace')) && request.cookies.has('token')){
+        const response = NextResponse.next();
+        return response;
+    } else {
+        return NextResponse.json({error: 404},  {
+            status: 404,
+        });
+    }
+  }
 
 export const config = { matcher: ["/api/private/:path*"] }
