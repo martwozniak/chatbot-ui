372c47dd7314f966ab4d66f1ceca72e328194cd3
Marcin Wozniak <marcindamianwozniak@gmail.com>
2023-06-28 09:17:31 +0200

    feat: Add JWT Token support

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -5,6 +5,7 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
 import { Adapter } from "next-auth/adapters";
 import { signJwtAccessToken } from "@/utils/lib/jwt";
+import { randomBytes, randomUUID } from "crypto";
 
 const prisma = new PrismaClient();
 
@@ -22,10 +23,30 @@ export default NextAuth({
         })
         
     ],
-    secret: process.env.SECRET,
+    secret: process.env.SECRET as string,
     session: {
-        strategy: 'jwt'
-    },    
+        // Choose how you want to save the user session.
+        // The default is `"jwt"`, an encrypted JWT (JWE) stored in the session cookie.
+        // If you use an `adapter` however, we default it to `"database"` instead.
+        // You can still force a JWT session by explicitly defining `"jwt"`.
+        // When using `"database"`, the session cookie will only contain a `sessionToken` value,
+        // which is used to look up the session in the database.
+        strategy: "jwt",
+      
+        // Seconds - How long until an idle session expires and is no longer valid.
+        maxAge: 30 * 24 * 60 * 60, // 30 days
+      
+        // Seconds - Throttle how frequently to write to database to extend a session.
+        // Use it to limit write operations. Set to 0 to always update the database.
+        // Note: This option is ignored if using JSON Web Tokens
+        updateAge: 24 * 60 * 60, // 24 hours
+        
+        // The session token is usually either a random UUID or string, however if you
+        // need a more customized session token string, you can define your own generate function.
+        generateSessionToken: () => {
+          return randomUUID?.() ?? randomBytes(32).toString("hex")
+        }
+      },       
     jwt: {
     // The maximum age of the NextAuth.js issued JWT in seconds.
     // Defaults to `session.maxAge`.
@@ -42,17 +63,10 @@ export default NextAuth({
           }
           return token
         },
-        async session({ session, user, token }) {
-            // I skipped the line below coz it gave me a TypeError
-            //session.accessToken = token.accessToken;
-            session = {
-                ...session,
-                user: {
-                    id: user.id,
-                    ...session.user
-                }
-            }
-            return session
-          },
+        async session({session,     token}) {
+            session.user.id = token.id
+            session.user.accessToken = token;
+            return session 
+        },
       }      
 })
