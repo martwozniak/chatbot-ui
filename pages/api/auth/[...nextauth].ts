4b5937f9ceb02cfb743756ff5ed885aa247d3156
Marcin Wozniak <marcindamianwozniak@gmail.com>
2023-06-29 06:03:08 +0200

    feat(JWT): Add next-auth config and types

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,8 +1,15 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google";
 import GithubProvider from "next-auth/providers/github"
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+import { Adapter } from "next-auth/adapters";
+import { randomBytes, randomUUID } from "crypto";
+
+const prisma = new PrismaClient();
 
 export default NextAuth({
+    adapter: PrismaAdapter(prisma) as Adapter<boolean>,
     // Configure one or more authentication providers
     providers: [
         GoogleProvider({
@@ -12,7 +19,33 @@ export default NextAuth({
         GithubProvider({
             clientId: process.env.GITHUB_ID as string,
             clientSecret: process.env.GITHUB_SECRET as string,
-        }),
-        // ...add more providers here
+        })
+        
     ],
+    secret: process.env.SECRET as string,
+    session: {
+        strategy: "jwt",
+        maxAge: 30 * 24 * 60 * 60, // 30 days
+        updateAge: 24 * 60 * 60, // 24 hours
+        generateSessionToken: () => {
+          return randomUUID?.() ?? randomBytes(32).toString("hex")
+        }
+      },       
+    jwt: {
+    maxAge: 60 * 60 * 24 * 30,
+    },
+    callbacks: {
+        async jwt({ token, account, user }) {
+          if (account) {
+            token.accessToken = account.access_token
+            token.id = user?.id
+          }
+          return token
+        },
+        async session({session, token}) {
+            session.user.id = token.id
+            session.user.accessToken = token;
+            return session 
+        },
+      }      
 })
